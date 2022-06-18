b86f01028f8b084b1cbd11a35a61f937f9cd10a3
MKujawa1 <maciej.kujawa96@o2.pl>
2022-06-18 18:40:01 +0200

    Fixed filtering, added ability to sort

--- GUI.py
+++ GUI.py
@@ -3,8 +3,10 @@ import pandas as pd
 ### Load data
 global data
 data = pd.read_excel('new_data.xlsx')
+
 global edit_data 
 edit_data = data
+
 global categories
 categories = []
 for i in range(len(data)):
@@ -12,46 +14,10 @@ for i in range(len(data)):
     for k in range(len(gen)):
         categories.append(gen[k])
 categories = list(set(categories))
+categories = sorted(categories)
+
 global keys
 keys = list(data.keys())
-
-def cell_edit(sender, app_data):
-
-    # remember current cell for later
-    global current_cell
-    current_cell = app_data[1]
-    # show clicked info to user
-    
-    # store cell contents for later
-    cell_content = dpg.get_value(app_data[1])
-    print(cell_content)
-
-    # save current parent
-    parent = dpg.get_item_parent(current_cell)
-    # iterate cells in current row and save the position
-    cell_position = None
-    cell_before = None
-    for child in dpg.get_item_children(parent)[1]:
-        # if we saved a cell position, we can save the next one for the before-parameter
-        if cell_position:
-            cell_before = dpg.get_item_alias(child)
-            # escape to not overwrite any saved items
-            break
-        if dpg.get_item_alias(child) == current_cell:
-            cell_position = dpg.get_item_alias(child)
-    # delete current cell (meaning the label)
-    dpg.delete_item(current_cell)
-    # remove the alias if not already happened
-    if dpg.does_alias_exist(current_cell):
-        dpg.remove_alias(current_cell)
-    
-    # add an input text widget instead of the label one
-    # if we saved a "before"-position, attach it before that position
-    if cell_before:
-        dpg.add_input_text(tag=current_cell, parent=parent, before=cell_before)
-    else:
-        dpg.add_input_text(tag=current_cell, parent=parent)
-    dpg.set_value(current_cell, cell_content)
 
 def add_row():
     # rows = 0
@@ -59,10 +25,7 @@ def add_row():
     for j in range(len(edit_data[:200])):
         with dpg.table_row(parent="maintable"):
             for i in range(len(keys)):
-                dpg.add_text(str(edit_data.iloc[rows][i]), tag=f"cell_{rows}{i}{j}")
-                with dpg.item_handler_registry(tag=f"cell_handler_{rows}{i}{j}") as handler:
-                    dpg.add_item_clicked_handler(callback=cell_edit)
-                dpg.bind_item_handler_registry(f"cell_{rows}{i}{j}", f"cell_handler_{rows}{i}{j}")
+                dpg.add_text(str(edit_data.iloc[rows][i]), tag=f"cell_{rows}{i}{j}")      
         rows += 1
 
 def delete_table():
@@ -92,14 +55,10 @@ def create_table():
                    borders_outerH=True, borders_innerV=True, borders_innerH=True, borders_outerV=True,
                    tag="maintable", row_background=True):
 
-        
         w = [20,3,3,4,4,18]
         for i in range(len(keys)):
             dpg.add_table_column(tag=f"col_{i}", label=f"{keys[i]}",
                                  init_width_or_weight=w[i])
-
-        
-        
         add_row()
 
 def reset():
@@ -111,9 +70,8 @@ def reset():
     create_table()
 
 def filtering(sender):
-    # test = data.drop(data[(data['genre']=='Komedia')].index)
-    global edit_data
-    
+
+    global edit_data    
     global data
     edit_data = data
     min_year = int(dpg.get_value('year_start'))
@@ -125,21 +83,52 @@ def filtering(sender):
     min_want = int(dpg.get_value('want_start'))
     max_want = int(dpg.get_value('want_stop'))
     cat = dpg.get_value('gen')
-    print(min_year)
     
-    edit_data = edit_data.drop(edit_data[(edit_data[keys[1]]<=min_year)&(edit_data[keys[1]]>=max_year)].index)
-    edit_data = edit_data.drop(edit_data[(edit_data[keys[2]]<=min_rate)&(edit_data[keys[2]]>=max_rate)].index)
-    edit_data = edit_data.drop(edit_data[(edit_data[keys[3]]<=min_pop)&(edit_data[keys[3]]>=max_pop)].index)   
-    edit_data = edit_data.drop(edit_data[(edit_data[keys[4]]<=min_want)&(edit_data[keys[4]]>=max_want)].index)
+    edit_data = edit_data.drop(edit_data[(edit_data[keys[1]]<=min_year)|(edit_data[keys[1]]>=max_year)].index)
+    edit_data = edit_data.drop(edit_data[(edit_data[keys[2]]<=min_rate)|(edit_data[keys[2]]>=max_rate)].index)
+    edit_data = edit_data.drop(edit_data[(edit_data[keys[3]]<=min_pop)|(edit_data[keys[3]]>=max_pop)].index)   
+    edit_data = edit_data.drop(edit_data[(edit_data[keys[4]]<=min_want)|(edit_data[keys[4]]>=max_want)].index)
     ind = []
     for i in range(len(edit_data)):
-        if (cat in edit_data['genre'][i])==False:
+        if (cat in edit_data['genre'][edit_data.index[i]])==False:
             ind.append(i)
     edit_data = edit_data.drop(edit_data.index[ind])
+        
     
     delete_table()
     create_table()
     
+def sort_as():
+    sort_val = dpg.get_value('radio')
+    global edit_data
+    if sort_val == 'year':
+        edit_data = edit_data.sort_values(by = 'year',ascending = True)
+    elif sort_val == 'rate':
+        edit_data = edit_data.sort_values(by = 'rate',ascending = True)
+    elif sort_val == 'pop rate':
+        edit_data = edit_data.sort_values(by = 'pop_rate',ascending = True)
+    elif sort_val == 'want see':
+        edit_data = edit_data.sort_values(by = 'want_see',ascending = True)
+    
+    
+    delete_table()
+    create_table()
+    
+def sort_ds():
+    sort_val = dpg.get_value('radio')
+    global edit_data
+    if sort_val == 'year':
+        edit_data = edit_data.sort_values(by = 'year',ascending = False)
+    elif sort_val == 'rate':
+        edit_data = edit_data.sort_values(by = 'rate',ascending = False)
+    elif sort_val == 'pop rate':
+        edit_data = edit_data.sort_values(by = 'pop_rate',ascending = False)
+    elif sort_val == 'want see':
+        edit_data = edit_data.sort_values(by = 'want_see',ascending = False)
+    
+    delete_table()
+    create_table()
+
 dpg.create_context()
 with dpg.window(tag = 'Main'):
     with dpg.child_window(tag = 'Filter',parent = 'Main'):
@@ -177,13 +166,18 @@ with dpg.window(tag = 'Main'):
         
         
     with dpg.child_window(tag = 'Sort',parent = 'Main'):
-        pass
+        dpg.add_text("SORT DATA BY")
+        dpg.add_separator()
+        dpg.add_radio_button(['year','rate','pop rate','want see'],tag = 'radio',default_value='year')
+        dpg.add_button(tag= 'as',label = 'ascending',callback=sort_as)
+        dpg.add_same_line()
+        dpg.add_button(tag= 'ds',label = 'descending',callback=sort_ds)
     with dpg.child_window(tag = 'Table',parent = 'Main'):
         create_table()
         
             
 dpg.set_primary_window('Main',True)
-dpg.create_viewport(title = 'Filmweb Management', width=1300, height=600,min_height=600,min_width=1300)
+dpg.create_viewport(title = 'Filmweb Management', width=1300, height=600,min_height=600,min_width=1300,x_pos = 10,y_pos = 10)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 
@@ -218,6 +212,14 @@ while dpg.is_dearpygui_running():
     dpg.set_item_pos('fd', pos=(int(w_f*0.5),43))
     dpg.set_item_pos('fd_text', pos=(8,43))
     dpg.set_item_width('fd', int(w_f*0.36))
+    
+    w_s = dpg.get_item_width('Sort')
+    h_s = dpg.get_item_height('Sort')
+    
+    dpg.set_item_pos('ds', pos=(int(w_s*0.5),h_s-27))
+    dpg.set_item_width('ds', int(w_s*0.36))
+    dpg.set_item_pos('as', pos=(8,h_s-27))
+    dpg.set_item_width('as', int(w_s*0.36))
    
     
 dpg.destroy_context()
